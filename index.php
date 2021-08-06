d4d562d3ff1f1e12523b159de75cedb5bf125565
Mohammad Haseeb Ashiq <learndotpk@gmail.com>
2021-08-06 11:03:23 -0700

    complete website

---
+++ index.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./main.css">
+    <script src="https://kit.fontawesome.com/07d7b7e553.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- Navbar -->
+
+    <div id="menu" class="menu">
+        <div id="menu-bar" onclick="onClickMenu()">
+        <div id="bar1" class="bar"></div>
+        <div id="bar2" class="bar"></div>
+        <div id="bar3" class="bar"></div>
+        <ul id="nav" class="nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </div>
+    </div>
+
+    <!-- navbar end -->
+    <!-- header -->
+
+    <header class="header">
+       <div class="hero">
+           <h1 class="title">Food For Health</h1>
+           <a href="#" class="hero-button pulsate">Book a table</a>
+       </div>
+    </header>
+
+    <!-- header end -->
+
+    <!-- about us -->
+
+    <section id="about">
+        <div>
+            <h2 class="title-text">About us</h2>
+        </div>
+        <div class="about-center">
+            <!-- start of the article -->
+
+            <article class="about">
+                <div class="about-icon">
+                    <i class="fas fa-mug-hot"></i>
+                </div>
+                <div class="about-text">
+                    <h2 class="about-subtitle">Drinks</h2>
+                    <p class="about-info">Lorem ipsum dolor sit amet consectetur,
+                         adipisicing elit. Optio, sequi?</p>
+                </div>
+            </article>
+
+            <!-- article end -->
+
+            <!-- start of the article -->
+
+            <article class="about">
+                <div class="about-icon">
+                    <i class="fas fa-utensils"></i>
+                </div>
+                <div class="about-text">
+                    <h2 class="about-subtitle">Healthy Food</h2>
+                    <p class="about-info">Lorem ipsum dolor sit amet consectetur,
+                         adipisicing elit. Optio, sequi?</p>
+                </div>
+            </article>
+
+            <!-- article end -->
+
+            <!-- start of the article -->
+
+            <article class="about">
+                <div class="about-icon">
+                    <i class="fas fa-mortar-pestle"></i>
+                </div>
+                <div class="about-text">
+                    <h2 class="about-subtitle">Organic Food</h2>
+                    <p class="about-info">Lorem ipsum dolor sit amet consectetur,
+                         adipisicing elit. Optio, sequi?</p>
+                </div>
+            </article>
+
+            <!-- article end -->
+
+            <!-- start of the article -->
+
+            <article class="about">
+                <div class="about-icon">
+                    <i class="fas fa-drumstick-bite"></i>
+                </div>
+                <div class="about-text">
+                    <h2 class="about-subtitle">White Meat</h2>
+                    <p class="about-info">Lorem ipsum dolor sit amet consectetur,
+                         adipisicing elit. Optio, sequi?</p>
+                </div>
+            </article>
+
+            <!-- article end -->
+
+            <!-- start of the article -->
+
+            <article class="about">
+                <div class="about-icon">
+                    <i class="fas fa-fish"></i>
+                </div>
+                <div class="about-text">
+                    <h2 class="about-subtitle">Sea Food</h2>
+                    <p class="about-info">Lorem ipsum dolor sit amet consectetur,
+                         adipisicing elit. Optio, sequi?</p>
+                </div>
+            </article>
+
+            <!-- article end -->
+
+            <!-- start of the article -->
+
+            <article class="about">
+                <div class="about-icon">
+                    <i class="fas fa-papper-hot"></i>
+                </div>
+                <div class="about-text">
+                    <h2 class="about-subtitle">Hot & Spicy</h2>
+                    <p class="about-info">Lorem ipsum dolor sit amet consectetur,
+                         adipisicing elit. Optio, sequi?</p>
+                </div>
+            </article>
+
+            <!-- article end -->
+        </div>
+    </section>
+
+    <!-- about us end -->
+
+<!-- menu section -->
+
+<section class="menu" id="menu">
+
+    <article class="menu-image"></article>
+    <article class="menu-text">
+        <div class="menu-text-center">
+            <h1>Our Menu</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis itaque repudiandae 
+                laboriosam aperiam ducimus earum labore inventore? Odit, deserunt ratione.</p>
+        <a href="#">Explore</a>
+            </div>
+    </article>
+
+</section>
+
+<!-- menu section end -->
+
+<!-- social icon -->
+
+<section id="social-icons">
+    <a href="#"><i class="fab fa-facebook facebook"></i></a>
+    <a href="#"><i class="fab fa-twitter twitter"></i></a>
+    <a href="#"><i class="fab fa-instagram instagram"></i></a>
+    <a href="#"><i class="fab fa-google-plus google-plus"></i></a>
+</section>
+
+<!-- social icon end -->
+
+<!-- numbers -->
+
+<section id="numbers">
+    <article class="number">
+      <i class="fas fa-cloud-meatball"></i>
+      <p>50</p>
+      <h3>Meat Dishes</h3>
+    </article>
+
+    <article class="number">
+        <i class="fas fa-cheese"></i>
+        <p>35</p>
+        <h3>Cheese Dishes</h3>
+      </article>
+
+      <article class="number">
+        <i class="fas fa-pizza-slice"></i>
+        <p>25</p>
+        <h3>Pizzas</h3>
+      </article>
+
+      <article class="number">
+        <i class="fas fa-ice-cream"></i>
+        <p>50</p>
+        <h3>Desserts</h3>
+      </article>
+
+</section>
+
+<!-- numbers end -->
+
+<!-- card section -->
+
+<section id="food">
+    <div>
+        <h2 class="title-text">Food Fuusion</h2>
+    </div>
+    <div class="food-container">
+        <!-- articles -->
+        <article class="food-card">
+            <img src="./images/hero.jpg" class="food-img" alt="">
+            <div class="img-text">
+                <h1>Breakfast</h1>
+            </div>
+            <div class="img-footer">
+                <div class="footer-icon">
+                    <i class="fas fa-dollar-sign">25</i>
+                </div>
+                <div class="footer-btn">
+                    <button type="button" class="food-btn">Order Now</button>
+                </div>
+            </div>
+        </article>
+        <!-- articles end -->
+
+         <!-- articles -->
+         <article class="food-card">
+            <img src="./images/pizzas.jpg" class="food-img" alt="">
+            <div class="img-text">
+                <h1>Dinner</h1>
+            </div>
+            <div class="img-footer">
+                <div class="footer-icon">
+                    <i class="fas fa-dollar-sign">45</i>
+                </div>
+                <div class="footer-btn">
+                    <button type="button" class="food-btn">Order Now</button>
+                </div>
+            </div>
+        </article>
+        <!-- articles end -->
+
+         <!-- articles -->
+         <article class="food-card">
+            <img src="./images/burger.jpg" class="food-img" alt="">
+            <div class="img-text">
+                <h1>Burger</h1>
+            </div>
+            <div class="img-footer">
+                <div class="footer-icon">
+                    <i class="fas fa-dollar-sign">25</i>
+                </div>
+                <div class="footer-btn">
+                    <button type="button" class="food-btn">Order Now</button>
+                </div>
+            </div>
+        </article>
+        <!-- articles end -->
+
+       
+    </div>
+</section>
+
+<!-- end card section -->
+
+<!-- Gallary Section -->
+
+<section id="gallery">
+    <div>
+        <h2 class="title-text">
+            Main Cuisines
+        </h2>
+    </div>
+    <div id="gallery-center">
+        <article class="gallery-item">
+            <a href="./images/pizzas.jpg">
+            <img src="./images/pizzas.jpg" alt="">
+            </a>
+        </article>
+
+        <article class="gallery-item">
+            <a href="./images/burger.jpg">
+            <img src="./images/burger.jpg" alt="">
+            </a>
+        </article>
+
+        <article class="gallery-item">
+            <a href="./images/hero.jpg">
+            <img src="./images/hero.jpg" alt="">
+            </a>
+        </article>
+
+        <article class="gallery-item">
+            <a href="./images/plate.jpg">
+            <img src="./images/plate.jpg" alt="">
+            </a>
+        </article>
+    </div>
+</section>
+
+<!-- Gallary Section end -->
+
+<!-- footer section -->
+
+<footer class="footer">
+    <div class="section-center">
+        <p class="footer-text">
+            &copy;<span>Food For Health</span>
+        </p>
+    </div>
+</footer>
+
+<!-- footer section end -->
+<!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="./magnific/jquery.magnific-popup.js"></script>
+<script src="./main.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#gallery-center').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  // other options
+  gallery:{
+         enabled:true
+  }
+});
+});
+</script>
+</body>
+</html>
