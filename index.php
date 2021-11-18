97cfc00d6c35af0f03b63dd326f4a2fe3c64b7a6
Mohammad Haseeb Ashiq <learndotpk@gmail.com>
2021-11-18 23:30:21 +0500

    change again

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
        <div class="hero">
             <!-- <h1 class="title">Food For Health</h1> -->
             <h1 class="title">my whatsapp please contact +923205790020</h1>
+            <!-- <h1 class="title">my email haseebahmad7862@gmail.com</h1> -->
            <a href="#" class="hero-button pulsate">Book a table</a>
        </div>
     </header>
